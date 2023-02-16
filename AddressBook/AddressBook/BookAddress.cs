a9dd39fbf15f76ccf1939ced11e69812cf854e22
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-17 02:37:07 +0530

    [Rohan]UC1: Creating contact

---
+++ AddressBook/AddressBook/BookAddress.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class BookAddress
+    {
+        public void CreateContact()
+        {
+            ContactPerson contact = new ContactPerson()
+            {
+                FirstName = "Rohan",
+                LastName = "Shewale",
+                Address = "L/12",
+                City = "Mumbai",
+                State = "Mh",
+                PhoneNumber = "98696346",
+                Zip = "34545",
+                Email = "Rohan@.com"
+            };
+            Display(contact);
+        }
+        public void Display(ContactPerson contact)
+        {
+            Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+            Console.WriteLine($"Address: {contact.Address} {contact.City} {contact.State} {contact.Zip}");
+            Console.WriteLine($"Phone: {contact.PhoneNumber}, Email: {contact.Email}");
+        }
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*________AddressBook Program_______*");
+            Console.WriteLine("Press any key to create contact");
+            Console.ReadKey();
+            BookAddress book = new BookAddress();
+            book.CreateContact();
+
+            Console.ReadKey();
+        }
+    }
+}
